daf571b962ea8a0f95c86c8777f45dda14bca044
sangeeta2003 <mishrasangeeta944@gmail.com>
2023-11-27 10:56:12 +0530

    add google image

--- pages/login.js
+++ pages/login.js
@@ -5,9 +5,12 @@ export default function LoginPage({ providers }) {
     <div className="flex items-center justify-center h-screen">
       {Object.values(providers).map((provider) => (
         <div key={provider.id}>
-          <button onClick={() => signIn(provider.id)}>
+          <button className="bg-twitterwhite pl-3 pr-5 px-5 px-2 text-black rounded-full flex items-center" >
+          <img src='google_icon.png' className='h-8'/>
             Sign in with {provider.name}
+            
           </button>
+          
         </div>
       ))}
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,13 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      colors:{
+        twitterwhite :'#e7e9ea',
+        twitterblue :'#308CD8',
+        twitterBorder:'#2f3336',
+        twitterLightGray:'#71767b',
+        twitterDarkGray:'#17181C',
+      }
     },
   },
   plugins: [],
