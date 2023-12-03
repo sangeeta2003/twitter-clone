e57ff16749ca70f743790c640372d920b197486d
sangeeta2003 <mishrasangeeta944@gmail.com>
2023-12-03 22:00:50 +0530

    add

--- pages/api/users.js
+++ pages/api/users.js
@@ -1,10 +1,14 @@
 import mongoose from "mongoose";
 import { initmongoose } from "@/lib/mongoose";
 import User from "../../models/user";
+import { unstable_batchedUpdates } from "react-dom";
+import { unstable_getServerSession } from "next-auth";
 export default async function handle(req,res){
     await initmongoose();
+    unstable_getServerSession(req,res)
     if(req.method === 'PUT'){
         const {username} = req.body;
+        User.findByIdAndUpdate()
         res.json({username});
     }
     if(req.mathod === 'GET')
