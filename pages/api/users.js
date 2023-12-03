93aadb8d1bd38d05814a8082c095d4abfebfa293
sangeeta2003 <mishrasangeeta944@gmail.com>
2023-12-03 21:55:27 +0530

    change

--- pages/api/users.js
+++ pages/api/users.js
@@ -3,8 +3,15 @@ import { initmongoose } from "@/lib/mongoose";
 import User from "../../models/user";
 export default async function handle(req,res){
     await initmongoose();
-    const id = req.query.id;
-    const User = await User.findById(id)
-    res.json({User});
+    if(req.method === 'PUT'){
+        res.json('ok');
+    }
+    if(req.mathod === 'GET')
+    {
+        const id = req.query.id;
+        const User = await User.findById(id)
+        res.json({User});
+    }
+    
 
 }
